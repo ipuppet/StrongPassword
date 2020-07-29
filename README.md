db2ced35ccf47c2f458c2721c04fb8b93c21b5d3
zimingw <zimingw.ol@outlook.com>
2020-07-30 00:12:17 +0800

    master

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 ## Strong Password
 
-=======
-## Strong Password
-
->>>>>>> 0a1b00b45c83e9fe790ee5c8312707e94c1028fc
 一个强密码生成器
--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Generator = require("./generator").Generator
 const Storage = require("./storage").Storage
 const UI = require("./ui").UI
@@ -26,33 +25,4 @@ function run() {
 
 module.exports = {
     run: run
-=======
-const Generator = require("./generator").Generator
-const Storage = require("./storage").Storage
-const UI = require("./ui").UI
-
-class Kernel {
-    generator = null
-    storage = null
-
-    constructor() {
-        this.generator = new Generator()
-        this.storage = new Storage()
-    }
-
-    generate_strong_password() {
-        return this.generator.generate()
-    }
-}
-
-function run() {
-    // 实例化应用核心
-    let kernel = new Kernel()
-    // 渲染UI
-    new UI(kernel).render()
-}
-
-module.exports = {
-    run: run
->>>>>>> 0a1b00b45c83e9fe790ee5c8312707e94c1028fc
 }
