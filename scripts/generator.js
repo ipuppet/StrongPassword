6c7d9de8dbfbb032174afa86ab62bf60414a7654
zimingw <zimingw.ol@outlook.com>
2020-07-30 00:13:32 +0800

    master

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Generator {
     part = null
     length = null
@@ -36,43 +35,4 @@ class Generator {
 
 module.exports = {
     Generator: Generator
-=======
-class Generator {
-    part = null
-    length = null
-    connector = null
-    character_set = null
-
-    constructor() {
-        this.update()
-    }
-
-    update() {
-        this.part = $prefs.get("settings.generator.part")
-        this.length = $prefs.get("settings.generator.length")
-        this.connector = $prefs.get("settings.generator.connector") ? "-" : ""
-        this.character_set = $prefs.get("settings.generator.character_set")
-    }
-
-    _generate_part() {
-        let result = ""
-        for (let i = 0; i < this.length; i++) {
-            result += this.character_set[Math.floor(Math.random() * this.character_set.length)]
-        }
-        return result
-    }
-
-    generate() {
-        this.update()
-        var result = [];
-        for (let i = 0; i < this.part; i++) {
-            result.push(this._generate_part())
-        }
-        return result.join(this.connector)
-    }
-}
-
-module.exports = {
-    Generator: Generator
->>>>>>> 0a1b00b45c83e9fe790ee5c8312707e94c1028fc
 }
